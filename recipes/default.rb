169a3736994be086fd82c9106e4305cce08444cf
karuru6225 <e2hjhi@gmail.com>
2014-08-03 01:42:57 +0900

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: bash
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+data_ids = data_bag('users')
+data_ids.each do |id|
+	u = data_bag_item('users', id)
+	user u['id'] do
+		home u['home']
+		action [:create]
+	end
+	directory u['home'] + '/.ssh' do
+		owner u['id']
+		group u['id']
+		mode '0700'
+		action [:create]
+	end
+	template u['home'] + '/.ssh/authorized_keys' do
+		owner u['id']
+		group u['id']
+		mode '0600'
+		source 'authorized_keys.erb'
+		variables({
+			:pubkey => u['ssh-keys']
+		})
+		action [:create]
+	end
+end
